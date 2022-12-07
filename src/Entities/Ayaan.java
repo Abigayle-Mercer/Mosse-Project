c9ebef9e8de19573217138a6d7d75da7276a1a07
Abigayle Mercer <abigayle.mercer@gmail.com>
2022-12-06 16:42:39 -0800

    ayaan class is done?

--- src/Entities/Ayaan.java
+++ src/Entities/Ayaan.java
@@ -1,6 +1,14 @@
 package Entities;
 
+import java.util.Random;
+
+import Entity_Attributes.Animates;
 import Entity_Attributes.Entity_I;
+import Entity_Attributes.Move;
+import Entity_Attributes.Transformable;
+import Pathing.AStarPathingStrategy;
+import Pathing.PathingStrategy;
+import Pathing.SingleStepPathingStrategy;
 import Starter_Classes.EventScheduler;
 import Starter_Classes.ImageStore;
 import Starter_Classes.Point;
@@ -9,34 +17,12 @@ import processing.core.PImage;
 
 import java.util.List;
 
-public class Ayaan implements Entity_I {
-    @Override
-    public List<PImage> getImages() {
-        return null;
-    }
+public class Ayaan extends Move{
 
-    @Override
-    public int getImageIndex() {
-        return 0;
-    }
 
-    @Override
-    public String getId() {
-        return null;
-    }
 
-    @Override
-    public void setPosition(Point position) {
-
-    }
-
-    @Override
-    public Point getPosition() {
-        return null;
-    }
-
-    @Override
-    public void setImageIndex(int i) {
+    public Ayaan(String id, Point position, List<PImage> images, double actionPeriod) {
+        super(id, position, images, actionPeriod);
 
     }
 
@@ -44,4 +30,19 @@ public class Ayaan implements Entity_I {
     public void scheduleActions(EventScheduler scheduler, WorldModel world, ImageStore imageStore) {
 
     }
+
+
+    @Override
+    public Point nextPosition(WorldModel world, Point destPos) {
+        PathingStrategy ps = new SingleStepPathingStrategy();
+        List<Point> path = ps.computePath(this.getPosition(), destPos, (Point p) -> (world.withinBounds(p) && ((!world.isOccupied(p)) || (world.getOccupancyCell(p).getClass() == STUMP.class))),
+                Move::adjacent, PathingStrategy.CARDINAL_NEIGHBORS);
+
+
+        Random rand = new Random(); //instance of random class
+        int index = rand.nextInt(0, 3);
+        return path.get(index);
+    }
+
+
 }
